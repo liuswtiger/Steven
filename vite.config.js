4d504d6ca33b5606c2ba200287314d5f6ad05d62
liuswtiger <58280718+liuswtiger@users.noreply.github.com>
2025-09-23 16:58:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 强制 Vite 内联所有 CSS 和 JS 文件
+    assetsInlineLimit: 4096,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      }
+    }
+  }
 })
